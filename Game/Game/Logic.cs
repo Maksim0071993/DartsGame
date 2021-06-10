99c39b8e7e2b4f9f586fd08ec2e31579fb64f510
Maksim Loginov <maksim08@list.ru>
2021-06-10 19:04:10 +0300

    input

---
+++ Game/Game/Logic.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Logic
+    {
+        
+        
+        
+
+       private string UserChoise(string message)
+        {
+           string choise;
+            switch (message)
+            {
+                
+                case "Камень": 
+                    Console.WriteLine("Ваш выбор Камень");
+                    choise = "Камень";
+                    return choise;
+                case "Ножницы":
+                    Console.WriteLine("Ваш выбор Ножницы");
+                    choise = "Ножницы";
+                    return choise;
+                case "Бумага":
+                    Console.WriteLine("Ваш выбор Бумага");
+                    choise = "Бумага";
+                    return choise;
+                default:
+                    Console.WriteLine("Неверный ввод. Допустимые варианты : Камень, Ножницы, Бумага");
+                    break;
+            }
+            return choise;
+
+        }
+    }
+}
---
+++ Game/Game/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет! Сыграем в игру ?");
+            Console.WriteLine("Выбирай — камень, ножницы или бумага ? Для завершения игры, введи — выход");
+            string userChoise = Console.ReadLine();//переменная содержащая выбор пользователя
+                                       //прописываем завершение программы
+            
+            
+            String[] items = new String[3];
+            items[0] = "Stone";
+            items[1] = "Scissors";
+            items[2] = "Paper";
+        }
+    }
+}
